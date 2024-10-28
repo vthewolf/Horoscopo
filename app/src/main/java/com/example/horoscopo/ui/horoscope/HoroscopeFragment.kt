f8e5ba91c49f9a44fe39b4d12aa6b7a15e0276e7
VictorSantiagoFerrera <victor.santiago@the-cocktail.com>
2024-10-28 16:47:12 +0100

    feat: add viewbinding and creation of first fragment

---
+++ app/src/main/java/com/example/horoscopo/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.horoscopo.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horoscopo.R
+
+class HoroscopeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_horoscope, container, false)
+    }
+}
