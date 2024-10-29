60c979d73cf3ef7c917a4c87d687cbc3c961b29d
VictorSantiagoFerrera <victor.santiago@the-cocktail.com>
2024-10-29 13:07:04 +0100

    feat: prepare all classes to be injectable

--- app/src/main/java/com/example/horoscopo/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/horoscopo/ui/home/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.horoscopo.R
 import com.example.horoscopo.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
