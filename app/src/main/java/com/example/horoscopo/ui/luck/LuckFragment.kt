48bb1979aa8a609839653f62585c023295ad8572
VictorSantiagoFerrera <victor.santiago@the-cocktail.com>
2024-10-29 09:08:40 +0100

    feat: create all app fragments

--- app/src/main/java/com/example/horoscopo/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscopo/ui/luck/LuckFragment.kt
@@ -1,22 +1,22 @@
-package com.example.horoscopo.ui.horoscope
+package com.example.horoscopo.ui.luck
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.horoscopo.databinding.FragmentHoroscopeBinding
+import com.example.horoscopo.databinding.FragmentLuckBinding
 
-class HoroscopeFragment : Fragment() {
+class LuckFragment : Fragment() {
 
-    private var _binding: FragmentHoroscopeBinding? = null
+    private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 }
--- app/src/main/java/com/example/horoscopo/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horoscopo/ui/palmistry/PalmistryFragment.kt
@@ -1,22 +1,23 @@
-package com.example.horoscopo.ui.horoscope
+package com.example.horoscopo.ui.palmistry
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.horoscopo.databinding.FragmentHoroscopeBinding
+import com.example.horoscopo.databinding.FragmentPalmistryBinding
 
-class HoroscopeFragment : Fragment() {
+class PalmistryFragment : Fragment() {
 
-    private var _binding: FragmentHoroscopeBinding? = null
+    private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
 }
